3cdea3622c361285f838b96e9c471ccc4541aaa5
Pierre-Luc Nichols <pierrelucnichols@gmail.com>
2014-11-03 14:55:44 +0000

    Implement ngDialog module

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
-angular.module('myApp', [])
-	.controller('myPerson', function($scope){
+angular.module('myApp', ['ngDialog'])
+	.controller('myPerson', ['$scope', 'ngDialog', function($scope, ngDialog){
 		$scope.person = {};
 		$scope.person.description = "This is my name and an image representing me!";
 		$scope.person.image = "default-avatar";
-	});
+
+		ngDialog.open({
+			template: '<p>my template</p>',
+			className: 'ngdialog-theme-plain',
+			plain: true
+		});
+	}]);
