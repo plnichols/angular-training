bc4a0befce265cc18c1f2648583015113f001b03
Pierre-Luc Nichols <pierrelucnichols@gmail.com>
2014-11-03 14:09:46 +0000

    Add multiple controllers with separate scope

--- js/app.js
+++ js/app.js
@@ -1,17 +1,6 @@
 angular.module('myApp', [])
 	.controller('myPerson', function($scope){
 		$scope.person = {};
-		$scope.person.name = "Pierre";
 		$scope.person.description = "This is my name and an image representing me!";
 		$scope.person.image = "default-avatar";
-	})
-	.controller('myCalculator', function($scope){
-		$scope.calc = {};
-		$scope.calc.num1 = 1;
-		$scope.calc.num2 = 2;
-
-		$scope.resetCalculator = function(){
-			$scope.calc.num1 = 0;
-			$scope.calc.num2 = 0;
-		}
 	});
