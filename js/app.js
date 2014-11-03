12c82bc3c48146532252f026476a12c42781a136
Pierre-Luc Nichols <pierrelucnichols@gmail.com>
2014-11-03 15:38:06 +0000

    Add karma test runner

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 angular.module('myApp', ['ngDialog'])
 	.controller('myPerson', ['$scope', 'ngDialog', function($scope, ngDialog){
 		$scope.person = {};
+		$scope.person.name = "Name";
 		$scope.person.description = "This is my name and an image representing me!";
 		$scope.person.image = "default-avatar";
 
