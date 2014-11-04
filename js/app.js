e952e88ae859ec20ff3f0d02cc9077d2eca7516e
Pierre-Luc Nichols <pierrelucnichols@gmail.com>
2014-11-04 15:53:10 +0000

    Implement Flickr feed with jsonp

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 angular.module('myApp', [])
-	.controller('myPeople', function($scope, $http){
-		$scope.people = {};
-		
-		$http.get('ajax/people.json')
-			.success(function(data){
-				$scope.people = data;
-			})
+	.controller('myFlickrFeed', function($scope, $http){
+
+		$scope.searchTerm = 'cat';
+		$scope.images = {};
+
+		$scope.getJsonp = function(){
+			$http.jsonp('http://api.flickr.com/services/feeds/photos_public.gne?tags=' + $scope.searchTerm + '&tagmode=any&format=json&jsoncallback=JSON_CALLBACK')
+				.success(function(data){
+					$scope.images = data.items;
+				});
+		}
+
+		$scope.$watch('searchTerm', function() {
+			$scope.getJsonp();
+		});
 	});
