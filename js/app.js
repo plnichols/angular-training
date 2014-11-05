bfa4774e86e32dd9a004828d25ddf70704652ae7
Pierre-Luc Nichols <pierrelucnichols@gmail.com>
2014-11-05 14:40:38 +0000

    Create directive for profile section

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 angular.module('myApp', ['ngDialog'])
-	.controller('myPerson', ['$scope', 'ngDialog', function($scope, ngDialog){
+	.controller('myPerson', ['$scope', 'ngDialog', function($scope){
 		$scope.person = {};
 		$scope.person.description = "This is my name and an image representing me!";
 		$scope.person.image = "default-avatar";
-
-		$scope.openDialog = function(){
-			ngDialog.open({
-				template: '<p>my template</p>',
-				plain: true
-			});
-		}
-	}]);
+	}])
+	.directive('profileDirective', function(){
+		return {
+			scope: true,
+			templateUrl: 'template-profile.html',
+			// calling controller here because otherwise, the directive doesn't have a person object
+			// by calling the controller, we get a instance of myPerson with its scope
+			controller: 'myPerson'
+		};
+	});
